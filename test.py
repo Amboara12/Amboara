13ed731077f0095c29b48839f152fa024069fe99
unknown <rakotomaroamboarara12@gamil.com>
2022-03-04 11:51:56 -0800

    first commit

--- test.py
+++ test.py
@@ -3,6 +3,9 @@ a = int(input("entrer la valeur de a"))
 b = int(input("entrez la valeur de b"))
 c = int(input("entrez la valeur de c "))
 delta = (b*b)-(4*a*c)
+print("delta=", delta)
+racine = pow(delta, 1/2)
+print("racine=" + str(racine))
 if delta < 0:
     print("delta negative alors pas de solutoin dans R")
 if delta > 0:
@@ -12,5 +15,6 @@ if delta > 0:
     x2 = (-b - int(pow(delta, 1/2))) / (2*a)
     print("x2=" + str(x2))
 if delta == 0:
-    print("solution double " "x1 = x2 ")
-    x1 = x2 = (-b) / (2*a)
+    print("delta egal 0 alors il existe une solution double x1=x2")
+    x = (-b / (2*a))
+    print("x=" + str(x))
